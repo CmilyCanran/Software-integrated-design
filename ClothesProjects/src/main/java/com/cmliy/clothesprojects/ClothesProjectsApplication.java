7835199379e2047419e06806f10775454abffccb
Cmily <282987762@qq.com>
2025-10-09 11:31:08 +0800

    项目初始

---
+++ ClothesProjects/src/main/java/com/cmliy/clothesprojects/ClothesProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.cmliy.clothesprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesProjectsApplication.class, args);
+	}
+
+}
---
+++ ClothesProjects/src/test/java/com/cmliy/clothesprojects/ClothesProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmliy.clothesprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
